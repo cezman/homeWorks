8c702f3c6c3c2fde56849f9ec35a71e4d8073dcf
Tsyren Balmaev <balmaev@gmail.com>
2022-09-05 16:55:40 +0300

    minor fix

--- task1/task1/ViewController.swift
+++ task1/task1/ViewController.swift
@@ -1,29 +1,21 @@
-//
-//  ViewController.swift
-//  task1
-//
-//  Created by Tsyren Balmaev on 02.09.2022.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
     
-    var counter: Int = 0
+    private var counter: Int = 0
 
-    @IBOutlet weak var counterLabel: UILabel!
-    @IBOutlet weak var counterButton: UIButton!
+    @IBOutlet private var counterLabel: UILabel!
+    @IBOutlet private var counterButton: UIButton!
    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         counterLabel.text = ("Значение счетчика: \(counter)")
         counterButton.tintColor = .darkGray
         counterButton.setTitle("Push me", for: .normal)
 
     }
     
-    @IBAction func counterTouch(_ sender: UIButton) {
+    @IBAction private func counterTouch(_ sender: UIButton) {
         counter += 1
         counterLabel.text = ("Значение счетчика: \(counter)")
     }
